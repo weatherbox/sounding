4cfc4934ff3cd669f372e5ee074e1013a8a097cf
tattii <yuta.tatti@gmail.com>
2018-02-20 19:20:21 +0900

    init map

---
+++ map.js
@@ -0,0 +1,17 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoidGF0dGlpIiwiYSI6ImNqMWFrZ3ZncjAwNmQzM3BmazRtNngxam8ifQ.DNMc6j7E4Gh7UkUAaEAPxA';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/tattii/cj8fozpkv0jn22rnwn6h65hvc',
+    zoom: 4.2,
+    center: [136.6, 35.5],
+    attributionControl: false,
+    logoPosition: 'bottom-right',
+    hash: true,
+    localIdeographFontFamily: "'Hiragino Kaku Gothic ProN', 'ヒラギノ角ゴ ProN W3', Meiryo, メイリオ, sans-serif"
+});
+
+map.on('load', function() {
+    //window.amedas = new AmedasGL(map);
+});
+
